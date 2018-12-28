0f00b5d4c7ee7c9a78c3736e2033c2cd014e70f6
Your NameVijay <vijay8484patil@gmail.com>
2018-12-28 22:03:15 +0530

    added Assignmnment63

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,9 +2,16 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+
+  template: `
+    <button (click)="onClickMe()">Upper</button>
+    {{clickMessage}}`
+
 })
 export class AppComponent {
-  title = 'Assignment63';
+   clickMessage = 'Marvellous Infosystems';
+
+   onClickMe() {
+     this.clickMessage = 'Educating for Better tomorrow';
+   }
 }
